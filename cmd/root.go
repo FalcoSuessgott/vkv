03354540f94e669566af500889be6476717e866d
Tom Morelly <tom-morelly@gmx.de>
2021-11-28 14:58:02 +0100

    wip

--- cmd/root.go
+++ cmd/root.go
@@ -3,41 +3,69 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"sort"
 	"strings"
-
-	"github.com/spf13/cobra"
+	"text/tabwriter"
 
 	"github.com/FalcoSuessgott/vkv/pkg/vault"
 	"github.com/ghodss/yaml"
+	"github.com/spf13/cobra"
 )
 
-const directorySuffix = "/"
+const (
+	DELIMITER       = "/"
+	SEPERATOR       = " "
+	NEW_LINE        = "\n"
+	MASK_CHAR       = "*"
+	DEFAULT_KV_PATH = "kv2"
+)
 
-// Secrets map containing all readable secrets
-type Secrets map[string]interface{}
+var defaultWriter = os.Stdout
 
-// Options holds all available commandline options
+// Secrets map containing all readable secrets.
+type (
+	Secrets map[string]interface{}
+	Keys    []string
+)
+
+// Options holds all available commandline options.
 type Options struct {
-	rootPath string
-	subPath  string
-	list     bool
-	restore  bool // import is a golang reserved key word
-	json     bool
-	yaml     bool
+	rootPath    string
+	subPath     string
+	writer      io.Writer
+	onlyKeys    bool
+	onlyPaths   bool
+	showSecrets bool
+	json        bool
+	yaml        bool
+}
+
+func defaultOptions() *Options {
+	return &Options{
+		rootPath:    DEFAULT_KV_PATH,
+		showSecrets: false,
+		writer:      defaultWriter,
+	}
 }
 
 func newRootCmd(version string) *cobra.Command {
-	o := &Options{}
+	o := defaultOptions()
 	s := Secrets{}
 
 	cmd := &cobra.Command{
 		Use:           "vkv",
-		Short:         "vault kv engine exporter and importer",
+		Short:         "recursively list secrets from Vaults KV2 engine",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := o.validateFlags(); err != nil {
+				return err
+			}
+
+			// READ
 			v, err := vault.NewClient()
 			if err != nil {
 				return err
@@ -45,12 +73,12 @@ func newRootCmd(version string) *cobra.Command {
 
 			s.listRecursive(v, o.rootPath, o.subPath)
 
-			if o.json && o.yaml {
-				return fmt.Errorf("cannot specify both --to-json and --to-yaml")
-			}
+			// MODIFY
+			o.evalModifyFlags(s)
 
+			// PRINT
 			if !o.json && !o.yaml {
-				fmt.Println(s.print())
+				o.print(s)
 			}
 
 			if o.json {
@@ -65,14 +93,18 @@ func newRootCmd(version string) *cobra.Command {
 		},
 	}
 
+	// Input
 	cmd.Flags().StringVarP(&o.rootPath, "root-path", "p", o.rootPath, "root path")
 	cmd.Flags().StringVarP(&o.subPath, "sub-path", "s", o.subPath, "sub path")
 
+	// Modify
+	cmd.Flags().BoolVar(&o.onlyKeys, "only-keys", o.onlyKeys, "print only keys")
+	cmd.Flags().BoolVar(&o.onlyPaths, "only-paths", o.onlyPaths, "print only paths")
+	cmd.Flags().BoolVar(&o.showSecrets, "show-secrets", o.showSecrets, "print out secrets")
+
+	// Output format
 	cmd.Flags().BoolVarP(&o.json, "to-json", "j", o.json, "print secrets in json format")
 	cmd.Flags().BoolVarP(&o.yaml, "to-yaml", "y", o.json, "print secrets in yaml format")
-
-	cmd.Flags().BoolVarP(&o.list, "list", "l", o.list, "list all secrets from a path")
-	cmd.Flags().BoolVarP(&o.restore, "import", "i", o.restore, "import all secrets")
 
 	return cmd
 }
@@ -86,6 +118,39 @@ func Execute(version string) error {
 	return nil
 }
 
+func (o *Options) validateFlags() error {
+	if o.json && o.yaml {
+		return fmt.Errorf("cannot specify both --to-json and --to-yaml\n")
+	}
+
+	if o.onlyKeys && o.showSecrets {
+		return fmt.Errorf("cannot specify both --only-keys and --show-secrets\n")
+	}
+
+	if o.onlyPaths && o.showSecrets {
+		return fmt.Errorf("cannot specify both --only-paths and --show-secrets\n")
+	}
+
+	if o.onlyKeys && o.onlyPaths {
+		return fmt.Errorf("cannot specify both --only-keys and --only-paths\n")
+	}
+	return nil
+}
+
+func (o *Options) evalModifyFlags(s Secrets) {
+	if o.onlyKeys {
+		s.onlyKeys()
+	}
+
+	if o.onlyPaths {
+		s.onlyPaths()
+	}
+
+	if !o.showSecrets {
+		s.maskSecrets()
+	}
+}
+
 func (s *Secrets) listRecursive(v *vault.Vault, rootPath, subPath string) {
 	keys, err := v.ListPath(rootPath, subPath)
 	if err != nil {
@@ -93,20 +158,15 @@ func (s *Secrets) listRecursive(v *vault.Vault, rootPath, subPath string) {
 	}
 
 	for _, k := range keys {
-		if strings.HasSuffix(k, directorySuffix) {
-			s.listRecursive(v, rootPath, subPath+directorySuffix+strings.TrimSuffix(k, directorySuffix))
+		if strings.HasSuffix(k, DELIMITER) {
+			s.listRecursive(v, rootPath, buildPath(subPath, k))
 		} else {
-			secrets, err := v.ReadSecrets(rootPath, subPath+directorySuffix+k)
+			secrets, err := v.ReadSecrets(rootPath, buildPath(subPath, k))
 			if err != nil {
 				log.Fatalf("error reading secret at %s/%s/%s: %v.\n", rootPath, subPath, k, err)
 			}
 
-			path := rootPath + directorySuffix + subPath + directorySuffix + k
-			if subPath == "" {
-				path = rootPath + directorySuffix + k
-			}
-
-			(*s)[path] = secrets
+			(*s)[buildPath(rootPath, subPath, k)] = secrets
 		}
 	}
 }
@@ -129,22 +189,121 @@ func (s *Secrets) toYAML() string {
 	return string(out)
 }
 
-func (s Secrets) sortKeys() []string {
-	keys := make([]string, 0, len(s))
-	for k := range s {
+func sortMapKeys(m map[string]interface{}) []string {
+	keys := make(Keys, 0, len(m))
+	for k := range m {
 		keys = append(keys, k)
 	}
 
-	sort.Strings(keys)
+	sort.Sort(keys)
 
 	return keys
 }
 
-func (s Secrets) print() string {
-	out := ""
-	for _, k := range s.sortKeys() {
-		out += fmt.Sprintf("%s:\t%v\n", k, s[k])
+func (k Keys) Len() int {
+	return len(k)
+}
+
+func (k Keys) Swap(i, j int) {
+	k[i], k[j] = k[j], k[i]
+}
+
+func (k Keys) Less(i, j int) bool {
+	k1 := strings.Replace(k[i], "/", "\x00", -1)
+	k2 := strings.Replace(k[j], "/", "\x00", -1)
+
+	return k1 < k2
+}
+
+func (o *Options) print(s Secrets) {
+	w := tabwriter.NewWriter(o.writer, 0, 8, 1, '\t', tabwriter.AlignRight)
+
+	fmt.Fprintf(w, "%s%s%s", o.rootPath, DELIMITER, NEW_LINE)
+
+	for _, k := range sortMapKeys(s) {
+		if o.onlyKeys {
+			fmt.Fprintf(w, "%s\t%v\n", k, printMap(s[k]))
+		} else if o.onlyPaths {
+			fmt.Fprintln(w, k)
+		} else {
+			fmt.Fprintf(w, "%s\t%v\n", k, printMap(s[k]))
+		}
 	}
 
-	return out
+	w.Flush()
+}
+
+func (s Secrets) onlyKeys() {
+	for k := range s {
+		m, ok := s[k].(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		for k := range m {
+			m[k] = ""
+		}
+	}
+}
+
+func (s Secrets) onlyPaths() {
+	for k := range s {
+		s[k] = nil
+	}
+}
+
+func (s Secrets) maskSecrets() {
+	for k := range s {
+		m, ok := s[k].(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		for k := range m {
+			secret := fmt.Sprintf("%v", m[k])
+			m[k] = strings.Repeat(MASK_CHAR, len(secret))
+		}
+	}
+}
+
+func buildPath(elements ...string) string {
+	p := ""
+
+	for i, e := range elements {
+		e = strings.TrimSuffix(e, DELIMITER)
+
+		if e == "" {
+			continue
+		}
+
+		p += e
+
+		if i < len(elements) {
+			p += DELIMITER
+		}
+	}
+
+	return strings.TrimSuffix(p, DELIMITER)
+}
+
+func printMap(m interface{}) string {
+	out := ""
+
+	secrets, ok := m.(map[string]interface{})
+	if !ok {
+		return ""
+	}
+
+	for _, k := range sortMapKeys(secrets) {
+		out += k
+
+		if secrets[k] == "" {
+			out += SEPERATOR
+		} else {
+			out += fmt.Sprintf("=%v ", secrets[k])
+		}
+
+	}
+
+	return strings.TrimSuffix(out, SEPERATOR)
 }
--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,49 +1,176 @@
 package cmd
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrint(t *testing.T) {
+func TestBuildPath(t *testing.T) {
 	testCases := []struct {
-		name   string
-		s      Secrets
-		output string
+		name     string
+		elements []string
+		expected string
 	}{
 		{
-			name: "test: normal map",
-			s: map[string]interface{}{
-				"key_1": "value",
-				"key_2": 12,
+			name:     "simple paths",
+			elements: []string{"root", "sub"},
+			expected: "root/sub",
+		},
+		{
+			name:     "simple sub paths",
+			elements: []string{"root", "sub", "sub2"},
+			expected: "root/sub/sub2",
+		},
+		{
+			name:     "empty sub path",
+			elements: []string{"root", "", "sub"},
+			expected: "root/sub",
+		},
+		{
+			name:     "trailing slash",
+			elements: []string{"root", "sub/", "sub", "demo/"},
+			expected: "root/sub/sub/demo",
+		},
+	}
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.expected, buildPath(tc.elements...), tc.name)
+	}
+}
+
+func TestValidateFlags(t *testing.T) {
+	testCases := []struct {
+		name    string
+		options *Options
+		err     bool
+	}{
+		{
+			name:    "test: valid options",
+			options: defaultOptions(),
+		},
+		{
+			name: "test: yaml and json",
+			err:  true,
+			options: &Options{
+				json: true,
+				yaml: true,
 			},
-			output: `key_1:	value
-key_2:	12
+		},
+		{
+			name: "test: only keys and only paths",
+			err:  true,
+			options: &Options{
+				onlyKeys:  true,
+				onlyPaths: true,
+			},
+		},
+		{
+			name: "test: only keys and show secrets ",
+			err:  true,
+			options: &Options{
+				onlyKeys:    true,
+				showSecrets: true,
+			},
+		},
+		{
+			name: "test: only paths and show secrets ",
+			err:  true,
+			options: &Options{
+				onlyPaths:   true,
+				showSecrets: true,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.options.validateFlags()
+
+		if tc.err {
+			assert.Error(t, err)
+			continue
+		}
+
+		assert.NoError(t, err)
+	}
+}
+
+func TestPrint(t *testing.T) {
+	testCases := []struct {
+		name    string
+		s       Secrets
+		options *Options
+		output  string
+	}{
+		{
+			name: "test: default opions",
+			s: map[string]interface{}{
+				"key_1": map[string]interface{}{"key": "value", "user": "password"},
+				"key_2": map[string]interface{}{"key": 12},
+			},
+			options: defaultOptions(),
+			output: `kv2/
+key_1	key=***** user=********
+key_2	key=**
 `,
 		},
 		{
-			name:   "test: empty map",
-			s:      map[string]interface{}{},
-			output: "",
-		},
-		// todo
-		{
-			name: "test: multiple values",
+			name: "test: show secrets",
 			s: map[string]interface{}{
-				"key_1": "value",
-				"key_2": 12,
-				"key_3": map[string]interface{}{"foo": "bar", "user": "password"},
+				"key_1": map[string]interface{}{"key": "value", "user": "password"},
+				"key_2": map[string]interface{}{"key": 12},
 			},
-			output: `key_1:	value
-key_2:	12
-key_3:	map[foo:bar user:password]
+			options: &Options{
+				rootPath:    "root",
+				showSecrets: true,
+			},
+			output: `root/
+key_1	key=value user=password
+key_2	key=12
+`,
+		},
+		{
+			name: "test: only paths",
+			s: map[string]interface{}{
+				"key_1": map[string]interface{}{"key": "value", "user": "password"},
+				"key_2": map[string]interface{}{"key": 12},
+			},
+			options: &Options{
+				rootPath:  "root",
+				onlyPaths: true,
+			},
+			output: `root/
+key_1
+key_2
+`,
+		},
+		{
+			name: "test: only keys",
+			s: map[string]interface{}{
+				"key_1": map[string]interface{}{"key": "value", "user": "password"},
+				"key_2": map[string]interface{}{"key": 12},
+			},
+			options: &Options{
+				rootPath: "root",
+				onlyKeys: true,
+			},
+			output: `root/
+key_1	key user
+key_2	key
 `,
 		},
 	}
 
 	for _, tc := range testCases {
-		assert.Equal(t, tc.output, tc.s.print(), tc.name)
+		var b bytes.Buffer
+
+		tc.options.writer = &b
+
+		tc.options.evalModifyFlags(tc.s)
+
+		tc.options.print(tc.s)
+		assert.Equal(t, tc.output, b.String(), tc.name)
 	}
 }
 
@@ -56,24 +183,19 @@ func TestSort(t *testing.T) {
 		{
 			name: "test: normal map",
 			s: map[string]interface{}{
-				"key_1": "value",
-				"key_2": 12,
+				"key_1":     map[string]interface{}{"key": "value", "user": "password"},
+				"key_2":     map[string]interface{}{"key": 12},
+				"key_1/a":   map[string]interface{}{"key": "value", "user": "password"},
+				"key_2/b":   map[string]interface{}{"key": 12},
+				"key_1/a/c": map[string]interface{}{"key": "value", "user": "password"},
+				"key_2/b/d": map[string]interface{}{"key": 12},
 			},
-			sortedKeys: []string{"key_1", "key_2"},
-		},
-		{
-			name: "test: normal map",
-			s: map[string]interface{}{
-				"key_3": nil,
-				"key_1": "value",
-				"key_2": 12,
-			},
-			sortedKeys: []string{"key_1", "key_2", "key_3"},
+			sortedKeys: []string{"key_1", "key_1/a", "key_1/a/c", "key_2", "key_2/b", "key_2/b/d"},
 		},
 	}
 
 	for _, tc := range testCases {
-		assert.Equal(t, tc.sortedKeys, tc.s.sortKeys(), tc.name)
+		assert.Equal(t, tc.sortedKeys, sortMapKeys(tc.s), tc.name)
 	}
 }
 
