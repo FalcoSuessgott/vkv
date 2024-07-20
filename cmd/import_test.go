170d72f3f1248c646977f7ddc19f47a888b41da8
Tom Morelly <tommorelly@gmail.com>
2024-07-20 18:10:36 +1000

    fix(import): test importing with existing secrets

--- cmd/import_test.go
+++ cmd/import_test.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/FalcoSuessgott/vkv/pkg/utils"
 )
@@ -175,5 +177,158 @@ func (s *VaultSuite) TestParseInput() {
 		if !tc.err {
 			s.Require().Equal(tc.expected, m, "expected "+tc.name)
 		}
+	}
+}
+
+func (s *VaultSuite) TestImportCommand() {
+	testCases := []struct {
+		name            string
+		importPath      string
+		exportPath      string
+		err             bool
+		originalSecrets string
+		newSecrets      string
+		expected        string
+	}{
+		{
+			name:       "simple merge of 2 secrets",
+			importPath: "yaml",
+			exportPath: "yaml",
+			originalSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			newSecrets: `yaml/:
+  secret2:
+    new: key
+`,
+			expected: `yaml/:
+  secret:
+    user: password
+  secret2:
+    new: key
+`,
+		},
+		{
+			name:       "merge of different nested secrets",
+			importPath: "yaml",
+			exportPath: "yaml",
+			originalSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			newSecrets: `yaml/:
+  secret2/:
+    sub/:
+      foo:
+        new: key
+`,
+			expected: `yaml/:
+  secret:
+    user: password
+  secret2/:
+    sub/:
+      foo:
+        new: key
+`,
+		},
+		{
+			name:       "write secrets to a subpath",
+			importPath: "yaml/secret2",
+			exportPath: "yaml",
+			originalSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			newSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			expected: `yaml/:
+  secret:
+    user: password
+  secret2/:
+    secret:
+      user: password
+`,
+		},
+		{
+			name:       "no original secrets",
+			importPath: "yaml",
+			exportPath: "yaml",
+			newSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			expected: `yaml/:
+  secret:
+    user: password
+`,
+		},
+		{
+			name:       "overwriting secrets",
+			importPath: "yaml",
+			exportPath: "yaml",
+			originalSecrets: `yaml/:
+  secret:
+    user: password
+`,
+			newSecrets: `yaml/:
+  secret:
+    new: key
+`,
+			expected: `yaml/:
+  secret:
+    new: key
+`,
+		},
+	}
+
+	for _, tc := range testCases {
+		//nolint: perfsprint, gosec
+		s.Run(tc.name, func() {
+			// 0. inject writer & vault client
+			writer = io.Discard
+
+			// 1. import original secrets
+			if tc.originalSecrets != "" {
+				o, err := os.CreateTemp(s.Suite.T().TempDir(), "original-secrets")
+				s.Require().NoError(err, "temp file")
+
+				s.Require().NoError(os.WriteFile(o.Name(), []byte(tc.originalSecrets), 0o644), "write original secrets")
+
+				importCmd := NewImportCmd()
+				importCmd.SetArgs([]string{fmt.Sprintf("-f=%s", o.Name())})
+				s.Require().NoError(importCmd.Execute(), "import original "+tc.name)
+			}
+
+			// 2. import new secrets
+			n, err := os.CreateTemp(s.Suite.T().TempDir(), "original-secrets")
+			s.Require().NoError(err, "temp file")
+
+			s.Require().NoError(os.WriteFile(n.Name(), []byte(tc.newSecrets), 0o644), "write new secrets")
+
+			importCmd := NewImportCmd()
+			importCmd.SetArgs([]string{fmt.Sprintf("-p=%s", tc.importPath), "--force", fmt.Sprintf("-f=%s", n.Name())})
+			s.Require().NoError(importCmd.Execute(), "import new "+tc.name)
+
+			// 3. export & assert
+			b := bytes.NewBufferString("")
+			writer = b
+
+			exportCmd := NewExportCmd()
+			exportCmd.SetArgs([]string{fmt.Sprintf("-p=%s", tc.exportPath), "-f=yaml", "--show-values"})
+
+			err = exportCmd.Execute()
+
+			if tc.err {
+				s.Require().Error(err, "export "+tc.name)
+			} else {
+				// if no error - compare exported secrets with expected value
+				out, _ := io.ReadAll(b)
+
+				s.Require().Equal(tc.expected, string(out), "secrets "+tc.name)
+			}
+		})
 	}
 }
